650f31dae0c7817257ee629a02a29383c78019bf
LilianaM1997 <112982364+LilianaM1997@users.noreply.github.com>
2024-02-09 17:15:16 -0500

    2/9 Class Work Time

--- Project 1/Assets/Scripts/Card Function.cs
+++ Project 1/Assets/Scripts/Card Function.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CardFunction : MonoBehaviour
 {
+    public Vector3 ScaleInitial;
+    public Vector3 ScaleOver;
+    public Vector3 ScaleCurrent;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = Vector3.Lerp(transform.localScale, ScaleCurrent, .01f);
+    }
+    void OnMouseOver()
+    {
+
+        Debug.Log("Over");
+    }
+    void OnMouseEnter()
+    {
+        ScaleCurrent = ScaleOver;
+        Debug.Log("Enter");
+    }
+    void OnMouseExit()
+    {
+        ScaleCurrent = ScaleInitial;
+        Debug.Log("Exit");
     }
 }
--- Project 1/Assets/Scripts/Player Movement.cs
+++ Project 1/Assets/Scripts/Player Movement.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float XMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float YMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector2 move = new Vector2(XMove, YMove);
+        transform.Translate(move);
+
     }
+    
 }
