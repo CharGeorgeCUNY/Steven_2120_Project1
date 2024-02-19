90d52f27b62559761f2a1742523ff4b9a0f1c8b4
RedzPanda01 <89554966+RedzPanda01@users.noreply.github.com>
2024-02-19 01:19:58 -0500

    Project Done

--- Project 1/Assets/Scripts/Player Movement.cs
+++ Project 1/Assets/Scripts/Player Movement.cs
@@ -2,26 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 2f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private BoxCollider2D coll;
 
+    private float dirX = 0f;
+    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
 
-    // Start is called before the first frame update
+    int wholeNumber = 16;
+    float decimalNumber = 4.54f;
+    string text = "blabla";
+    bool boolean = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        float XMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float YMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        Vector2 move = new Vector2(XMove, YMove);
-        transform.Translate(move);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
+
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0);
+        }
     }
-    
+
+    private bool Grounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Card"))
+        {
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
+    }
 }
